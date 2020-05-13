7be02027a1645739d9435297ed87573b0ebe763e
CipherSamurai <63560659+CipherSamurai@users.noreply.github.com>
2020-05-13 14:37:36 +0000

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# 	helloWorld.py
+#  
+# 	Copyright 2020 
+#	@author <!pher $amur@!
+#	@date 5/13/2020
+#  
+
+
+def main(args):
+    print("Hello World")
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
